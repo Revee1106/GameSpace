c9c937c6f10ceaec5903b39b73452a2c7029bf89
Eric.Wang <100116122@qq.com>
2020-08-06 17:05:47 +0800

    first commit

---
+++ gamespace/src/main/java/com/eric/gamespace/taoism/Application.java
@@ -0,0 +1,13 @@
+package com.eric.gamespace.taoism;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+	
+}
---
+++ gamespace/src/main/java/com/eric/gamespace/taoism/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.eric.gamespace.taoism.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.extern.slf4j.Slf4j;
+
+@RestController
+@Slf4j
+public class UserController {
+
+}
