b19d529d5ba7933cb7581fa33fcdbf7010f06420
Eric.Wang <100116122@qq.com>
2020-08-10 14:07:08 +0800

    代码提交

--- gamespace/src/main/java/com/eric/gamespace/taoism/controller/UserController.java
+++ gamespace/src/main/java/com/eric/gamespace/taoism/controller/UserController.java
@@ -1,13 +1,32 @@
 package com.eric.gamespace.taoism.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.alibaba.fastjson.JSON;
+import com.eric.gamespace.taoism.common.util.JsonUtil;
+import com.eric.gamespace.taoism.service.UserService;
+import com.eric.gamespace.taoism.vo.user.UserRegistReqVo;
+import com.eric.gamespace.taoism.vo.user.UserRegistRespVo;
 
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
 @Slf4j
 public class UserController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@RequestMapping(value = "/userRegist")
+	public UserRegistRespVo userRegist(@RequestBody UserRegistReqVo reqVo) {
+		log.info("userRegist start, param is:{}", JsonUtil.beanToJson(reqVo));
+		UserRegistRespVo respVo = userService.userRegist(reqVo);
+		return null;
+		
+	}
 
 	@RequestMapping(value = "/")
 	public String home() {
--- gamespace/src/main/java/com/eric/gamespace/taoism/service/UserService.java
+++ gamespace/src/main/java/com/eric/gamespace/taoism/service/UserService.java
@@ -1,5 +1,15 @@
 package com.eric.gamespace.taoism.service;
 
+import com.eric.gamespace.taoism.vo.user.UserRegistReqVo;
+import com.eric.gamespace.taoism.vo.user.UserRegistRespVo;
+
 public interface UserService {
+
+	/**
+	 * 用户注册
+	 * @param reqVo
+	 * @return
+	 */
+	UserRegistRespVo userRegist(UserRegistReqVo reqVo);
 
 }
